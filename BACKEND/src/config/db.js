dd18256bea91366bf7a1c083b26c26a84e4bee37
Meet Hirpara <meetkumarhirpara@gmail.com>
2026-08-19 12:00:38 +0530

    remove unnecessary code

--- BACKEND/src/config/db.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-import CONFIG from '../config/config.js';
-
-async function connectDB() {
-    await mongoose.connect(CONFIG.MONGO_URL);
-    console.log('Connected to MongoDB');
-}
-
-export default connectDB;
--- BACKEND/src/routers/auth.routes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { registerValidator } from "../validators/auth.validators.js";
-
-const router = express.Router();
-
-router.post("/register", registerValidator);
-
-export default router;
